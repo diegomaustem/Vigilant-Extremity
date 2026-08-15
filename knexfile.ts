d8851efc1a254d4acf9877cd87ca98d9524e0802
diegomaustem <diego.maustem@gmail.com>
2026-08-14 21:43:58 -0300

    :sparkles: feat: Save

---
+++ knexfile.ts
@@ -0,0 +1,28 @@
+import type { Knex } from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL || {
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: Number(process.env.DB_PORT) || 5432,
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'secret',
+      database: process.env.DB_NAME || 'site_vigilant'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations',
+      extension: 'ts',
+    }
+}
+};
+
+export default config;
