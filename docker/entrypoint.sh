d698b6996bba2d7158cea4a1b11646bdbddc7305
diegomaustem <diego.maustem@gmail.com>
2026-08-24 20:59:14 -0300

    :sparkles: feat: Adjustment of entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 set -e
 
-echo "Carregando banco de dados."
+echo "🔍 Carregando banco de dados..."
 
 # Aguarda o PostgreSQL
 until nc -z -v -w30 "$DB_HOST" "$DB_PORT"; do
-  echo "Estabelecendo conexão com banco de dados $DB_HOST:$DB_PORT."
+  echo "⏳ Estabelecendo conexão com banco de dados $DB_HOST:$DB_PORT..."
   sleep 2
 done
-echo "Banco de dados pronto."
+echo "✅ Banco de dados pronto."
 
-echo "Executando migrações."
+# Aguarda o RabbitMQ
+echo "🔍 Aguardando RabbitMQ..."
+until nc -z -v -w30 rabbitmq 5672; do
+  echo "⏳ Aguardando RabbitMQ..."
+  sleep 2
+done
+echo "✅ RabbitMQ pronto!"
+
+echo "🔄 Executando migrações..."
 npm run knex migrate:latest
 
-echo "Iniciando aplicação."
+echo "🚀 Iniciando aplicação..."
 exec node dist/server.js
